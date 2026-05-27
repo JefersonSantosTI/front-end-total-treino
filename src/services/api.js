246de0da2ca4ddf4a392cf43371d0e7a6fc34f9d
JefersonSantosTI <jefersonbrandaosantos@gmail.com>
2026-05-27 20:49:42 -0300

    chat

--- src/services/api.js
+++ src/services/api.js
@@ -2,5 +2,5 @@ import axios from "axios"
 
 export const api = axios.create({
   // Adicionamos o /api no final da URL base
-  baseURL: "https://api.treinofit.app.br"
+  baseURL: "https://api.treinofit.app.br/api"
 })
