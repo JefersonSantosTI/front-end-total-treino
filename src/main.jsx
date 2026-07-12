fa448c603a366069dac1150a5f204c887963e8aa
Jeferson Santos <jefersonbrandaosantos@gmail.com>
2026-07-12 09:27:13 -0300

    definido

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom' // 🔥 1. Importamos o motor das rotas aqui!
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* 🔥 2. Abraçamos o App com o BrowserRouter */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
